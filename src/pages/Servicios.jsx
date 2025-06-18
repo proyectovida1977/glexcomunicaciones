792784d1577dfbec0fc2da6a4fa9f4a52009b6de
proyectovida1977 <proyectovida1977@gmail.com>
2025-06-17 18:14:56 -0600

    Pruebas navs

--- src/pages/Servicios.jsx
+++ src/pages/Servicios.jsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="text-center">
-      <h1 className="text-3xl font-bold mb-4">Bienvenido a Glex Comunicaciones</h1>
+      <h1 className="text-3xl font-bold mb-4">Servicios</h1>
       <p>Soluciones en telecomunicaciones, redes, CCTV, telefonía IP y más.</p>
     </div>
   );
